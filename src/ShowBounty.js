cf115302db9a90eb8dae2cf2a86b43758ce2a49c
brandiw <brandimichelle.0911@gmail.com>
2019-03-06 23:12:24 -0800

    Further simplifying

--- src/ShowBounty.js
+++ src/ShowBounty.js
@@ -17,9 +17,8 @@ class ShowBounty extends Component {
 	}
 
 	render(){
-		const status = this.state.bounty.captured?'CAUGHT':'AT LARGE'
 		return(
-			<div className='show-bounty'>
+			<div className="show-bounty">
 				<h2>{this.state.bounty.name}: ${this.state.bounty.reward}</h2>
 				<h4>Wanted For: {this.state.bounty.wantedFor}</h4>
 				<p>
@@ -28,7 +27,7 @@ class ShowBounty extends Component {
 				<p>
           Hunted by: {this.state.bounty.hunters.join(', ')} for {this.state.bounty.client}
         </p>
-				<h4>STATUS: {status}</h4>
+				<h4>STATUS: {this.state.bounty.captured ? 'CAUGHT' : 'AT LARGE'}</h4>
 			</div>
 		)
 	}
