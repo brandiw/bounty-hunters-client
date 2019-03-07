f13c06b58db829ff538d8f4c0fd95ac29851768e
brandiw <brandimichelle.0911@gmail.com>
2019-03-07 13:37:56 -0800

    Adding value field in inputs

--- src/NewBountyForm.js
+++ src/NewBountyForm.js
@@ -63,32 +63,32 @@ class NewBountyForm extends Component {
   			<form onSubmit={this.postBounty}>
   				<div className="form-control">
   					<label name="name">Name:</label>
-  					<input type="text" name="name" onChange={this.handleInput} />
+  					<input type="text" name="name" onChange={this.handleInput} value={this.state.name} />
   				</div>
   				<div className="form-control">
   					<label name="wantedFor">Crime:</label>
-  					<input type="text" name="wantedFor" onChange={this.handleInput} />
+  					<input type="text" name="wantedFor" onChange={this.handleInput} value={this.state.wantedFor} />
   				</div>
   				<div className="form-control">
   					<label name="client">Client:</label>
-  					<input type="text" name="client" onChange={this.handleInput} />
+  					<input type="text" name="client" onChange={this.handleInput} value={this.state.client} />
   				</div>
   				<div className="form-control">
   					<label name="reward">Reward: $</label>
-  					<input type="number" name="reward" onChange={this.handleInput} />
+  					<input type="number" name="reward" onChange={this.handleInput} value={this.state.reward} />
   				</div>
   				<div className="form-control">
   					<label name="ship">Ship:</label>
-  					<input type="text" name="ship" onChange={this.handleInput} />
+  					<input type="text" name="ship" onChange={this.handleInput} value={this.state.ship} />
   				</div>
   				<div className="form-control">
   					<label name="hunters">Hunters:</label>
-  					<input type="text" name="hunters" onChange={this.handleInput} />
+  					<input type="text" name="hunters" onChange={this.handleInput} value={this.state.hunters} />
   				</div>
   				<div></div>
   				<div className="form-control">
   					<label name="captured">Captured:</label>
-  					<input type="checkbox" name="captured" onChange={this.handleInput} />
+  					<input type="checkbox" name="captured" onChange={this.handleInput} value={this.state.captured} />
   					<input type="submit" value="Add New Bounty" />
   				</div>
   			</form>
